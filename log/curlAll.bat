14d6c7916ad6fa35ea30aa372207cb84e30d1cf1
peddie <paul.eddie@eeoc.gov>
2017-12-28 08:52:28 -0500

    good state

--- log/curlAll.bat
+++ log/curlAll.bat
@@ -1,8 +1,3 @@
-  curl -o prodIms1.log http://10.5.2.200/applogs/prod-nxg-logs-01/ims-nxg.log
-  curl -o prodIms2.log http://10.5.2.200/applogs/prod-nxg-logs-02/ims-nxg.log
-  curl -o prodIms3.log http://10.5.2.200/applogs/prod-nxg-logs-03/ims-nxg.log
-  curl -o prodServer1.log http://10.5.2.200/applogs/prod-nxg-logs-01/server.log
-  curl -o prodServer2.log http://10.5.2.200/applogs/prod-nxg-logs-02/server.log
-  curl -o prodServer3.log http://10.5.2.200/applogs/prod-nxg-logs-03/server.log
-  curl -o testIms.log http://10.5.2.200/applogs/tst-nxg-logs-01/ims-nxg.log
-  curl -o testServer.log http://10.5.2.200/applogs/tst-nxg-logs-01/server.log
+call _curlpims.bat
+call _curlpserver.bat
+call curltest.bat
